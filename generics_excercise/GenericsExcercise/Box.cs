3c7a6c8d690e97d40b4265d33c954bb93c36f384
Konstantin Kostenski <konstantin_kostenski@yahoo.com>
2021-02-16 21:29:39 +0200

    Added generics excercises.

---
+++ generics_excercise/GenericsExcercise/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsExcercise
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Data = new List<T>();
+        }
+
+        public List<T> Data { get; set; }
+
+        public void Swap(int index1, int index2)
+        {
+            T temp = this.Data[index1];
+            this.Data[index1] = this.Data[index2];
+            this.Data[index2] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var data in this.Data)
+            {
+                sb.AppendLine($"{data.GetType().FullName}: {data}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ generics_excercise/GenericsExcercise/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsExcercise
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int data = int.Parse(Console.ReadLine());
+                box.Data.Add(data);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(indexes[0], indexes[1]);
+            Console.WriteLine(box);
+
+        }
+    }
+}
